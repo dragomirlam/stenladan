e1802588207b1472c178c8dca1bffc40ce35b1c8
Lampros Dragomoiris <dragomirlam@gmail.com>
2014-07-07 22:55:51 +0200

    Addition of carrousel feauture

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -4,6 +4,12 @@
 		<meta charset="UTF-8">
 		<title>STENladan</title>
 		{{ HTML::style('css/style.css'); }}
+		
+		{{ HTML::style('css/gallery-css/dist/gallery.theme.css'); }}
+		{{ HTML::style('css/gallery-css/dist/gallery.prefixed.css'); }}
+		{{ HTML::style('css/gallery-css/vendor/normalize.css'); }}
+		<!-- {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js') }}
+		{{ HTML::script('responsiveslides.min.js') }} -->
 	</head>
 	<body>
 		<div id="header">
@@ -18,14 +24,35 @@
 	                <li><a href="#">Contact</a></li>
 	            </ul>
 	        </nav>
-	    </div>    
-    	<div id="top" height="50px">
-    		{{ HTML::image('images/stenladan.jpg', 'alt-text') }}
-    	</div>
-		<div id="content">
-			{{ HTML::image('images/background3.jpg', 'alt-text') }}
-		</div>
+	    </div>
+	    <div id="carousel">
+	    	<div class="gallery autoplay items-3">
+				<div id="item-1" class="control-operator"></div>
+				<div id="item-2" class="control-operator"></div>
+				<div id="item-3" class="control-operator"></div>
 
+				<figure class="item">
+				  {{ HTML::image('images/stenladan.jpg', 'alt-text') }}
+				</figure>
+
+				<figure class="item">
+				  <h1>Item 2</h1>
+				</figure>
+
+				<figure class="item">
+				  <h1>Item 3</h1>
+				</figure>
+
+				<div class="controls">
+				  <a href="#item-1" class="control-button">•</a>
+				  <a href="#item-2" class="control-button">•</a>
+				  <a href="#item-3" class="control-button">•</a>
+				</div>
+			</div>
+		</div>
+    	<div id="top">
+    		{{ HTML::image('images/party.jpg', 'alt-text') }}
+    	</div>
 		<div id="footer">
 		</div>
 	</body>
